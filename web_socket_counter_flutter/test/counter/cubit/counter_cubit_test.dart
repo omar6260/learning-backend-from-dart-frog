0bc4f326a8af3e7d51cf4fb47f70712408278c2e
Oumar <foumar404@gmail.com>
2023-05-24 19:00:12 +0200

    refactor: websocket counter flutter

--- web_socket_counter_flutter/test/counter/cubit/counter_cubit_test.dart
+++ web_socket_counter_flutter/test/counter/cubit/counter_cubit_test.dart
@@ -6,21 +6,21 @@ import 'package:web_socket_counter_flutter/counter/counter.dart';
 void main() {
   group('CounterCubit', () {
     test('initial state is 0', () {
-      expect(CounterCubit().state, equals(0));
+      expect(CounterCubit().state, (0));
     });
 
     blocTest<CounterCubit, int>(
       'emits [1] when increment is called',
       build: CounterCubit.new,
       act: (cubit) => cubit.increment(),
-      expect: () => [equals(1)],
+      expect: () => [(1)],
     );
 
     blocTest<CounterCubit, int>(
       'emits [-1] when decrement is called',
       build: CounterCubit.new,
       act: (cubit) => cubit.decrement(),
-      expect: () => [equals(-1)],
+      expect: () => [(-1)],
     );
   });
 }
